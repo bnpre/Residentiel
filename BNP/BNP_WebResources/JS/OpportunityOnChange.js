663f3d6022d3473974ac29fda7e9cab50529825b
bnpre <bnpre>
2017-02-24 11:01:53 +0530

    Test Delete

--- BNP/BNP_WebResources/JS/OpportunityOnChange.js
+++
@@ -1,8 +0,0 @@
-//Refresh "WebResource_MultiCanalContact" HTML Web Resource on Change of Contact Lookup
-function onContactChange() {
-  Xrm.Page.data.entity.save(null);
-  var wrRelatedOpportunities = Xrm.Page.ui.controls.get("WebResource_RelatedOpportunities");
-  var src = wrRelatedOpportunities.getSrc();
-  wrRelatedOpportunities.setSrc(null);
-  wrRelatedOpportunities.setSrc(src);
-}
